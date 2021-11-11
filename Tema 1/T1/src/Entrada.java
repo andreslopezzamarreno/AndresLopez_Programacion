778090b60a53d6dff8ae56f107b5886e2df4957f
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-11 20:11:23 +0100

    Create Entrada.java

---
+++ Tema 1/T1/src/Entrada.java
@@ -0,0 +1,22 @@
+public class Entrada {
+    //varibles de clases
+    String nombre = "Andres";
+    int edad = 18;
+    boolean carnet;
+
+    //metodos
+    public static void main(String[] args) {
+
+
+        // System.out.println(nombre);
+        new Entrada().saludar("Andres");
+        //saludar("pepe");
+        //saludar(nombre);
+    }
+
+    public void saludar(String nombreParam) {
+        System.out.println("hola bienvenido al programa de repaso " + nombreParam);
+        System.out.println(carnet);
+        System.out.println(edad);
+    }
+}
