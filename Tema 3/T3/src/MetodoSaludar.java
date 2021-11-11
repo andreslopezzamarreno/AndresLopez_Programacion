fca2f672892f048b539266c4d8e843accc071cee
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-11 16:05:37 +0100

    Create MetodoSaludar.java

---
+++ Tema 3/T3/src/MetodoSaludar.java
@@ -0,0 +1,11 @@
+public class MetodoSaludar {
+
+    public static void main(String args[]) {
+        saludar();
+    }
+
+    public static void saludar() {
+        System.out.println("Bienvenido al maravilloso mundo de la programación ");
+    }
+
+}
