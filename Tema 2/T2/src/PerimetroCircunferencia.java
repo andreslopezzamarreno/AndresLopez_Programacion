c0822896479b465fc7f072432f0c7a2c6c291d8d
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-11 16:01:10 +0100

    Create PerimetroCircunferencia.java

---
+++ Tema 2/T2/src/PerimetroCircunferencia.java
@@ -0,0 +1,10 @@
+public class PerimetroCircunferencia {
+    public static void main (String args[]) {
+
+        final double PI = 3.14159265;
+        int radio = 25;
+        double Perimetro = 2*PI*radio;
+        System.out.println("la circunferencia mide : " + Perimetro );
+
+    }
+}
