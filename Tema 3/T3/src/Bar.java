9094f3b2cd8a7bdf326cef62492d12a512976a3f
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-11 15:51:10 +0100

    update

---
+++ Tema 3/T3/src/Bar.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Bar {
+    public static void main(String args[]) {
+            Scanner in = new Scanner(System.in);
+
+            //declaracion de variables
+            int bebidas = 0;
+            int bocadillos = 0;
+
+            System.out.println("Numero de bebidas: ");
+            bebidas = in.nextInt();
+
+            System.out.println("Numero de bocadillos ");
+            bocadillos = in.nextInt();
+
+            //mostrar en pantalla
+            System.out.println("Coste de las bebidas: " + bebidas * 1.25);
+            System.out.println("coste de los bocadillos " + bocadillos * 2.05);
+            System.out.println("Coste del pedido: " + (bebidas * 1.25 + bocadillos * 2.05));
+
+            in.close();
+
+        }
+    }
