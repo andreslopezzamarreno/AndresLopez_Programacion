93be05812f6edf96a7261242b75f9d2686dd7243
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-11 15:57:17 +0100

    CamionCalabazas

---
+++ Tema 2/T2/src/CamionCalabazas.java
@@ -0,0 +1,13 @@
+public class CamionCalabazas {
+    public static void main(String args[]) {
+        
+        int caja = 8;
+        int camion1 = 3464;
+        int camion2 = camion1 - 664;
+        int camion3 = 220;
+
+        System.out.println("el camion 1 lleva " + camion1 / caja);
+        System.out.println("el camion 2 lleva " + camion2 / caja);
+        System.out.println("el camion 3 lleva " + camion3 * caja);
+    }
+}
