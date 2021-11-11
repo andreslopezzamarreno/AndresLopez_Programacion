c9ef8b1420ccbe5cd74ce1b24496846f7c16bc30
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-11 16:09:06 +0100

    MostrarPI

---
+++ Tema 3/T3/src/MostrarPi.java
@@ -0,0 +1,13 @@
+public class MostrarPi {
+
+    final static double PI = 3.14159265;
+    public static void main(String args[]) {
+
+        mostrarPi(PI);
+    }
+
+    public static double mostrarPi(double pi) {
+        System.out.println("El numero Pi es: " + PI);
+        return(PI);
+    }
+}
