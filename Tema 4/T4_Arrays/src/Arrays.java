d3b1436637719ef4898697a94983fe58c9f893e8
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-12 11:51:30 +0100

    update

--- Tema 4/T4_Arrays/src/Arrays.java
+++ Tema 4/T4_Arrays/src/Arrays.java
@@ -1,6 +1,6 @@
 public class Arrays {
     public static void main(String[] args) {
-        //array de diferentes variables
+       /* //array de diferentes variables
         int [] numeros =  new int[10];
         boolean [] aciertos = new boolean[5];
         String[] palabras = new String[3];
@@ -19,5 +19,37 @@ public class Arrays {
         for (int i = 0; i < numeros2.length; i++) {
             System.out.println(numeros2[i]);
         }
+
+        //modificar posicion del Array
+        numeros2[7] = 88;
+        System.out.println("modifico alguna posicion");
+        for (int i = 0; i < numeros2.length; i++) {
+            System.out.println(numeros2[i]);
+        }*/
+
+        //meter en un Array de 15 posiciones 15 aleatorios entre 0 -50
+        int [] numerosAleatorios= new int[15];
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            numerosAleatorios[i] = (int)(Math.random() * 51);
+            System.out.println(numerosAleatorios[i]);
+        }
+
+
+        int pares=0;
+        int impares=0;
+        for(int i = 0; i < numerosAleatorios.length; i++) {
+
+            if (numerosAleatorios[i] %2 == 0){
+                pares++;
+
+            }else{
+                impares++;
+            }
+        }
+        System.out.println("Hay " + pares + " numeros pares");
+        System.out.println("Hay " + impares + " numeros impares ");
+
+
+
     }
 }
