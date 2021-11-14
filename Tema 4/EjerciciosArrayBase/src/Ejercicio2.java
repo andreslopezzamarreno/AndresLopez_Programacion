d55896b4773aed4c81d8117ebd94bceedea288f0
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-14 12:12:03 +0100

    update

---
+++ Tema 4/EjerciciosArrayBase/src/Ejercicio2.java
@@ -0,0 +1,17 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int[] numeros = new int[30];
+        int suma = 0;
+        double media;
+
+        for (int i = 0; i < 30; i++) {
+            numeros[i] = (int) (Math.random() * 31);
+            suma += numeros[i];
+        }
+
+        media = (double) suma / (double)numeros.length;
+
+        System.out.println("La suma total de puntos es: " + suma);
+        System.out.println("La media de los puntos es: " + media);
+    }
+}
