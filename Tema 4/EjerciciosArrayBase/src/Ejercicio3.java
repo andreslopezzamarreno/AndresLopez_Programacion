3454902132582bb3ada567f5ba8492e8e5a054fd
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-14 12:23:10 +0100

    update

---
+++ Tema 4/EjerciciosArrayBase/src/Ejercicio3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int [] numerosAleatorios = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Introduce numero ");
+            numerosAleatorios[i] = sc.nextInt();
+        }
+        for (int i = 9; i >= 0; i--) {
+            System.out.println(numerosAleatorios[i]);
+
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
