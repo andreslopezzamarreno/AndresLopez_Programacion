35e5b9f9b88c826e164cfaa72e7bccc771f8b032
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-14 12:56:29 +0100

    update

--- Tema 4/EjerciciosArrayBase/src/Ejercicio5.java
+++ Tema 4/EjerciciosArrayBase/src/Ejercicio5.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Ejercicio5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int [] numerosAleatorios = new int[8];
+
+        for (int i = 0; i < 8; i++) {
+            System.out.println("Introduce numero ");
+            numerosAleatorios[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 8; i++) {
+            if (numerosAleatorios[i] %2 == 0) {
+                System.out.println(numerosAleatorios[i] + " par");
+            }else{
+                System.out.println(numerosAleatorios[i] + " impar");
+            }
+        }
+
+    }
 }
