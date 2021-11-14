3dab31ad81103943a6077243cb72ec8894945b3b
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-14 13:37:33 +0100

    update

--- Tema 4/EjerciciosArrayBase/src/Ejercicio6.java
+++ Tema 4/EjerciciosArrayBase/src/Ejercicio6.java
@@ -1,2 +1,29 @@
 public class Ejercicio6 {
+    public static void main(String[] args) {
+
+        int[] numeros = new int[20];
+        int modificaciones = 0;
+
+        for (int i = 0; i < 20; i++) {
+            numeros[i] = (int) (Math.random() * 31);
+        }
+
+        for (int i = 0; i < 20; i++) {
+            switch (numeros[i]) {
+                case 6:
+                    numeros[i] = 8;
+                    modificaciones++;
+                    break;
+                case 7:
+                    numeros[i] = 15;
+                    modificaciones++;
+                    break;
+                case 20:
+                    numeros[i] = 10;
+                    modificaciones++;
+                    break;
+            }
+        }
+        System.out.println("Se han hecho " + modificaciones + " modificaciones");
+    }
 }
