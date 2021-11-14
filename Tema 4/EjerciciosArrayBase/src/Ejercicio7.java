263a4e6d00ae9c18d4d93716e401f2f8fd6209b0
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-14 14:36:21 +0100

    update

--- Tema 4/EjerciciosArrayBase/src/Ejercicio7.java
+++ Tema 4/EjerciciosArrayBase/src/Ejercicio7.java
@@ -6,10 +6,12 @@ public class Ejercicio7 {
 
 
         String[] palabras = new String[10];
-        int opcion = -1;
-        int numeroLetras=0;
+        int opcion;
+        int numeroLetras;
         String palabra;
         double mediaLetras;
+        String maxPalabra = "";
+        String minPalabra = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
         for (int i = 0; i < 10; i++) {
             System.out.println("Introduce palabra:");
@@ -22,14 +24,17 @@ public class Ejercicio7 {
             opcion = sc.nextInt();
             switch (opcion){
                 case 1:
+                    //Mostrar todas las palabras
                     for (int i = 0; i < 10; i++) {
                         System.out.println(palabras[i]);
                     }
                     break;
                 case 2:
+                    //Obtener una palabra al azar
                     System.out.println(palabras[(int) (Math.random()*10)]);
                     break;
                 case 3:
+                    //Ver número de letras totales en el Array
                     numeroLetras = 0;
                     for (int i = 0; i < 10; i++) {
                         palabra = palabras[i];
@@ -38,6 +43,7 @@ public class Ejercicio7 {
                     System.out.println("El numero de letras totales es: " + numeroLetras);
                     break;
                 case 4:
+                    //Ver media de letras
                     numeroLetras = 0;
                     for (int i = 0; i < 10; i++) {
                         palabra = palabras[i];
@@ -47,7 +53,25 @@ public class Ejercicio7 {
                     System.out.println("La media de letras por palabras es " + mediaLetras);
                     break;
                 case 5:
-                    
+                    //Ver palabra con más letras
+                    for (int i = 0; i < 10; i++) {
+                        palabra = palabras[i];
+                        if (palabra.length() > maxPalabra.length()){
+                            maxPalabra = palabras[i];
+                        }
+                    }
+                    System.out.println("La palabra con mas letras es " + maxPalabra);
+                    break;
+                case 6:
+                    //Ver palabra con menos letras
+                    for (int i = 0; i < 10; i++) {
+                        palabra = palabras[i];
+                        if (palabra.length() < minPalabra.length()){
+                            minPalabra = palabras[i];
+                        }
+                    }
+                    System.out.println("La palabra con menos letras es " + minPalabra);
+                    break;
             }
         }while (opcion != 0);
     }
