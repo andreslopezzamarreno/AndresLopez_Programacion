8b4dd646643ffe214010d17b5d3f67fb01da892a
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-15 09:53:01 +0100

    Update Temario.java

--- Tema 4/Ejercicios/src/Temario.java
+++ Tema 4/Ejercicios/src/Temario.java
@@ -1,2 +1,8 @@
 public class Temario {
+    public static void main(String[] args) {
+        String [] temario = {"Tema 1: Introducción","Tema 2: Identificadores","Tema 3: Estructuras","Tema 4: Colecciones",
+                "Tema 5: Orientación a objetos", "Tema 6: Clases y Objetos", "Tema 7; Abstracción, Herencia y Polimorfismo",
+                "Tema 8: Ficheros y Excepciones", "Tema 9: Bases de datos", "Tema 10: Interfaces"};
+        
+    }
 }
