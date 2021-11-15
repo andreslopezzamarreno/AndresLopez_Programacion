999f4a21c617a96a5fbfcabaa132251164b48437
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-15 10:00:19 +0100

    Update MultiplicarArray.java

--- Tema 4/Ejercicios/src/MultiplicarArray.java
+++ Tema 4/Ejercicios/src/MultiplicarArray.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class MultiplicarArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int [] numeros = new int[10];
 
+        int [] numeros = new int[10];
+        int suma;
         for (int i = 0; i < numeros.length; i++) {
             System.out.println("Introduce numero");
             numeros[i] = sc.nextInt();
-            
+        }
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] *=2;
+            System.out.println(numeros[i]);
         }
     }
 }
