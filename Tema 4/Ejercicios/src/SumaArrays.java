6680c58cf5369bec56a9d07562742be948539840
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-15 11:17:37 +0100

    Create SumaArrays.java

---
+++ Tema 4/Ejercicios/src/SumaArrays.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SumaArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int posicion;
+        System.out.println("Que longitud quieres que tenga el Array ");
+        posicion = sc.nextInt();
+
+        int[] sumando1 = new int[posicion];
+        int[] sumando2 = new int[posicion];
+    }
+}
