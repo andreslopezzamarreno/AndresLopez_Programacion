d53073c1ed9d7abf4bdbf00c957fbfe7698f8447
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-15 09:15:58 +0100

    Update Ejercicio1.java

--- Tema 4/EjerciciosArrayBase/src/Ejercicio1.java
+++ Tema 4/EjerciciosArrayBase/src/Ejercicio1.java
@@ -4,15 +4,15 @@ public class Ejercicio1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] numerosAleatorios = new int[10];
+        int[] numeros = new int[10];
         int contador = -1;
         for (int i = 0; i < 10; i++) {
             System.out.println("Introduce numero ");
-            numerosAleatorios[i] = sc.nextInt();
+            numeros[i] = sc.nextInt();
         }
         for (int i = 0; i < 10; i++) {
             contador++;
-            System.out.println(contador + ")" + numerosAleatorios[i]);
+            System.out.println(contador + ")" + numeros[i]);
         }
     }
 }
