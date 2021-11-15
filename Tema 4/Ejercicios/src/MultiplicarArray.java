a272efcc80e8cf2dbc002e0468f2bfdba0b11e46
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-15 10:04:02 +0100

    update

--- Tema 4/Ejercicios/src/MultiplicarArray.java
+++ Tema 4/Ejercicios/src/MultiplicarArray.java
@@ -4,8 +4,9 @@ public class MultiplicarArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int [] numeros = new int[10];
-        int suma;
+        int [] numeros = new int[5];
+        int suma = 0;
+        double media = 0;
         for (int i = 0; i < numeros.length; i++) {
             System.out.println("Introduce numero");
             numeros[i] = sc.nextInt();
@@ -13,6 +14,10 @@ public class MultiplicarArray {
         for (int i = 0; i < numeros.length; i++) {
             numeros[i] *=2;
             System.out.println(numeros[i]);
+            suma += numeros[i];
+            media = (double) suma/(double)numeros.length;
         }
+        System.out.println("La suma de todos los numeros es " + suma);
+        System.out.println("La media de todos los numeros es " + media);
     }
 }
