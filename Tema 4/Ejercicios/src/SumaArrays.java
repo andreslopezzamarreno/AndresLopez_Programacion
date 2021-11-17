99da606960ad90a21560c3e2d3c2f8b135d68ca4
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-17 19:17:21 +0100

    update

--- Tema 4/Ejercicios/src/SumaArrays.java
+++ Tema 4/Ejercicios/src/SumaArrays.java
@@ -9,5 +9,19 @@ public class SumaArrays {
 
         int[] sumando1 = new int[posicion];
         int[] sumando2 = new int[posicion];
+        int [] resultado = new int[posicion];
+
+        for (int i = 0; i < posicion; i++) {
+            System.out.println("introcude el valor de la posicion " + i + " del primer array ");
+            sumando1[i] = sc.nextInt();
+        }
+        for (int i = 0; i < posicion; i++) {
+            System.out.println("introcude el valor de la posicion " + i + " del segundo array");
+            sumando2[i] = sc.nextInt();
+        }
+        for (int i = 0; i < posicion; i++) {
+            resultado[i] = sumando1[i] + sumando2[i];
+            System.out.println(resultado[i]);
+        }
     }
 }
