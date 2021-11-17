55be3c3c0f4f32e7d8fd93c17c4650491c735196
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-17 21:29:13 +0100

    update

---
+++ Tema 4/Ejercicios/src/FraseArray.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class FraseArray {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        String [] frase = new String[10];
+        System.out.println("Introduce una frase ");
+        for (int i = 0; i < frase.length; i++) {
+            frase[i] = sc.next();
+        }
+        for (int i = 0; i < frase.length; i++) {
+            System.out.println(frase[i]);
+        }
+    }
+}
--- Tema 4/Ejercicios/src/OperacionesArray.java
+++ Tema 4/Ejercicios/src/OperacionesArray.java
@@ -1,5 +1,5 @@
 public class OperacionesArray {
     public static void main(String[] args) {
-        
+
     }
 }
