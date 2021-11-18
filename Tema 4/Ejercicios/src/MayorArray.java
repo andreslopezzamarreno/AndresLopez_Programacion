5b53c42cbdda082db5ce5991d6be3e2b9000e5a9
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-18 09:33:20 +0100

    Update MayorArray.java

--- Tema 4/Ejercicios/src/MayorArray.java
+++ Tema 4/Ejercicios/src/MayorArray.java
@@ -4,13 +4,14 @@ public class MayorArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int [] enteros;
         int max = -999999999;
         int min = 999999999;
         int longitud;
         System.out.println("Que longitud quieres que tenga el Array");
         longitud = sc.nextInt();
 
-        int[] enteros = new int[longitud];
+        enteros = new int[longitud];
 
         for (int i = 0; i < enteros.length; i++) {
             System.out.println("Introduce valor para la posicion " + i);
