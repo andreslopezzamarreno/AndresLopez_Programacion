52bf79d8cc6b61f6ea2cbaaf2bba29cc14ff5222
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-19 12:02:04 +0100

    update

--- Tema 4/T4_Arrays/src/Multidimensionales.java
+++ Tema 4/T4_Arrays/src/Multidimensionales.java
@@ -1,15 +1,16 @@
 public class Multidimensionales {
     public static void main(String[] args) {
         int [][] arrayMulti = new int[3][3];
+        int [][] arrayMultiDiferente = {{1,2,3,4},{1,2,3},{4,5}};
         arrayMulti[0][0] = 7;
         arrayMulti[2][1] = 8;
         arrayMulti[1][2] = 6;
 
-        for (int i = 0; i < arrayMulti.length; i++) {
-            for (int j = 0; j < arrayMulti.length; j++) {
-                System.out.print(arrayMulti [i][j]);
+        for (int i = 0; i < arrayMultiDiferente.length; i++) {
+            for (int j = 0; j < arrayMultiDiferente[i].length; j++) {
+                System.out.print(arrayMultiDiferente [i][j] + " ");
             }
-            System.out.print(" \n");
+            System.out.println();
         }
     }
 }
