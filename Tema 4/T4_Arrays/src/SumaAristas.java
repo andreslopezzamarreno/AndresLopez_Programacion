c3a69dbc42ede19fb8264e1e1fd16e17ffb3d938
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-22 09:22:57 +0100

    Create SumaAristas.java

---
+++ Tema 4/T4_Arrays/src/SumaAristas.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumaAristas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int filas, columnas;
+        int numerosAleatorios[][];
+
+        System.out.println("Cuantas filas tendrá el Array");
+        filas = sc.nextInt();
+        System.out.println("Cuantas columnas tendrá el Array");
+        columnas = sc.nextInt();
+
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+
+
+        }
+    }
+}
