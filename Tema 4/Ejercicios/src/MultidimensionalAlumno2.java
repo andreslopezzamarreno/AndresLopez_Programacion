9fadb94681c799b74d2a757cdec8951ec6f08ff3
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-22 16:16:45 +0100

    update

--- Tema 4/Ejercicios/src/MultidimensionalAlumno2.java
+++ Tema 4/Ejercicios/src/MultidimensionalAlumno2.java
@@ -6,21 +6,38 @@ public class MultidimensionalAlumno2 {
 
         int opcion;
         int numAlumnos;
-        System.out.println("1.Definir el número de alumnos a registrar\n2.Registrar alumnos\n3.Ver datos de alumnos con nota media\n4.Ver datos de un alumno concreto\n5.Ver medias de la asignatura 1\n" +
-                "6.Ver medias de la asignatura 2\n7.Ver medias de la asignatura 3\n8.Salir");
-        while(opcion){
 
+        do{
+            System.out.println("1.Definir el número de alumnos a registrar\n2.Registrar alumnos\n3.Ver datos de alumnos con nota media\n4.Ver datos de un alumno concreto\n5.Ver medias de la asignatura 1\n" +
+                    "6.Ver medias de la asignatura 2\n7.Ver medias de la asignatura 3\n8.Salir");
+            opcion = sc.nextInt();
+
+        }while(opcion<1 && opcion<8);
+
+
+        switch (opcion){
+            case 1:
+                System.out.println("Numero de alumnos a registrar ");
+                numAlumnos = sc.nextInt();
+                if (numAlumnos<=1){
+                    System.out.println("Error");
+                }
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
         }
-        opcion = sc.nextInt();
 
-
-        switch (opcion);
-        System.out.println("Numero de alumnos a registrar ");
-        numAlumnos = sc.nextInt();
-        if (numAlumnos<=1){
-            System.out.println("Error") ;
-
-        }
 
     }
 }
