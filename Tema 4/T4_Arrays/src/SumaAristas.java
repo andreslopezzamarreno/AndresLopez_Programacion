35e0026150d24531b8e5414abef065c713ab3535
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-22 09:38:33 +0100

    update

--- Tema 4/T4_Arrays/src/SumaAristas.java
+++ Tema 4/T4_Arrays/src/SumaAristas.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class SumaAristas {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int filas, columnas;
         int numerosAleatorios[][];
 
@@ -10,13 +11,16 @@ public class SumaAristas {
         filas = sc.nextInt();
         System.out.println("Cuantas columnas tendrá el Array");
         columnas = sc.nextInt();
+        numerosAleatorios = new int[filas][ columnas];
 
         for (int i = 0; i < numerosAleatorios.length; i++) {
             for (int j = 0; j < numerosAleatorios[i].length; j++) {
-                
+                numerosAleatorios[i][j] =(int) (Math.random()*21);
+                System.out.print(numerosAleatorios[i][j] + " ");
             }
-
-
+            System.out.println();
         }
+
+        sc.close();
     }
 }
