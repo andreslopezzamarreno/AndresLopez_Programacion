31f61bec4f4581a50e877dfa9baad0f680d41fc6
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-22 16:07:57 +0100

    Update MultidimensionalAlumno2.java

--- Tema 4/Ejercicios/src/MultidimensionalAlumno2.java
+++ Tema 4/Ejercicios/src/MultidimensionalAlumno2.java
@@ -8,8 +8,12 @@ public class MultidimensionalAlumno2 {
         int numAlumnos;
         System.out.println("1.Definir el número de alumnos a registrar\n2.Registrar alumnos\n3.Ver datos de alumnos con nota media\n4.Ver datos de un alumno concreto\n5.Ver medias de la asignatura 1\n" +
                 "6.Ver medias de la asignatura 2\n7.Ver medias de la asignatura 3\n8.Salir");
+        while(opcion){
+
+        }
         opcion = sc.nextInt();
-        
+
+
         switch (opcion);
         System.out.println("Numero de alumnos a registrar ");
         numAlumnos = sc.nextInt();
