a06c97a448ea4f156f86ce148381c73155a3cff4
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-23 21:38:28 +0100

    update

--- Tema 4/Ejercicios/src/InvisibleArray.java
+++ Tema 4/Ejercicios/src/InvisibleArray.java
@@ -1,5 +1,39 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class InvisibleArray {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        String[] personas;
+        int[] pers;
+        int numPers;
+        int aleat;
+        int aleat2;
+
+
+        do {
+            System.out.println("Cuantas personas van a participar?(tiene que ser par)");
+            numPers = sc.nextInt();
+        } while (numPers % 2 != 0);
+        personas = new String[numPers];
+        pers = new int[numPers];
+
+        for (int i = 0; i < personas.length; i++) {
+            System.out.println("Introducir nombre del participante numero " + (i + 1));
+            personas[i] = sc.next();
+            pers[i] = i;
+            System.out.println(pers[i]);
+        }
+
+        for (int i = 0; i < personas.length / 2; i++) {
+            aleat = (int) (Math.random() * numPers);
+            aleat2 = (int) (Math.random() * numPers);
+            boolean contenido = Arrays.asList(pers).contains(2);
+            System.out.println(contenido);
+
+            System.out.println(personas[aleat] + " - " + personas[aleat2]);
+        }
     }
 }
--- Tema 4/Ejercicios/src/Loteria.java
+++ Tema 4/Ejercicios/src/Loteria.java
@@ -1,31 +1,30 @@
+
 import java.util.Scanner;
 
 public class Loteria {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int[] numeroIntro = new int[7];
         int[] boleto = new int[7];
         int suma = 0;
 
         for (int i = 0; i < boleto.length; i++) {
-            boleto[i] = (int) (Math.random()*10);
-            System.out.print(boleto[i]);
+            boleto[i] = (int) (Math.random() * 10);
         }
         for (int i = 0; i < numeroIntro.length; i++) {
             System.out.println("Introduce numero de la posicion " + i);
             numeroIntro[i] = sc.nextInt();
         }
         for (int i = 0; i < 7; i++) {
-            if (numeroIntro[i] == boleto[i]){
+            if (numeroIntro[i] == boleto[i]) {
                 suma++;
             }
         }
-        if (suma == 7){
+        if (suma == 7) {
             System.out.println("Has ganado la loteria");
-        }else{
+        } else {
             System.out.println("No has ganado la loteria");
         }
-
     }
 }
