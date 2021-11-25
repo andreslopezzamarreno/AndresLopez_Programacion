aa75a19cac3094c0d01424702b2e9236960c5989
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-25 19:48:39 +0100

    Update MovidaMultidimensional.java

--- Tema 4/Ejercicios/src/MovidaMultidimensional.java
+++ Tema 4/Ejercicios/src/MovidaMultidimensional.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 
+/* duda que surgió en clase de porque para recorrer el array multidimensional el segundo for alidado
+tenia que ser de la forma numeros[i].length y no numeros.length*/
+
+
 public class MovidaMultidimensional {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
@@ -7,7 +11,11 @@ public class MovidaMultidimensional {
         int [][] numeros = new int[3][4];
 
         for (int i = 0; i <  numeros.length; i++) {
-            for (int j = 0; j < numeros.length; j++) {
+            /*se pone numero[i].length por que en el caso de que el array no fuese un array cuadrado
+            la longitud de las columnas solo llegaria hasta la longitud de las filas y se quedarias valores
+            nulos o no podrias meter valores en las posiciones
+            */
+            for (int j = 0; j < numeros[i].length; j++) {
                 System.out.println("introduce valor de la posicion " );
                 numeros [i][j] = teclado.nextInt();
             }
