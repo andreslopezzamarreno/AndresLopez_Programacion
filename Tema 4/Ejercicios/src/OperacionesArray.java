4af281b1b727bade1d2822936fc103b208c78536
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-27 15:58:02 +0100

    update

--- Tema 4/Ejercicios/src/OperacionesArray.java
+++ Tema 4/Ejercicios/src/OperacionesArray.java
@@ -8,43 +8,55 @@ public class OperacionesArray {
         int[] numeros = new int[10];
         int posfin;
         for (int i = 0; i < numeros.length; i++) {
-            numeros[i] =(int) (Math.random() * 10 );
+            numeros[i] = (int) (Math.random() * 10);
 
-            if (i < numeros.length-1){
+            if (i < numeros.length - 1) {
                 System.out.print(numeros[i] + ", ");
-            }else{
+            } else {
                 System.out.print(numeros[i]);
             }
         }
+        System.out.println();
 
-        for (int i = 0; i < numeros.length; i++) {
-            /*if (numeros[i] %2 == 0){
-                numeros[i] ++;
-            }else{
-                numeros[i] --;
+
+        /*for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] % 2 == 0) {
+                numeros[i]++;
+            } else {
+                numeros[i]--;
             }
 
-            if(numeros[i]>0 && numeros[i] < 5){
-                numeros[i]*= 2;
-            }*/
 
-            //numeros[i] += ( -5 + (int) (Math.random()*11));
-
-            for (int item: numeros) {
-                System.out.println(item + ", ");
+            if (numeros[i] > 0 && numeros[i] < 5) {
+                numeros[i] *= 2;
             }
 
-            posfin = numeros[numeros.length -1];
-            if (i != 0){
-                numeros[i] = numeros[i-1];
+
+            numeros[i] += (-5 + (int) (Math.random() * 11));
+
+        }
+        posfin = numeros[numeros.length-1];
+        for (int i = numeros.length-1; i >= 0; i--) {
+            if (i != 0) {
+                numeros[i] = numeros[i - 1];
             } else {
                 numeros[0] = posfin;
             }
+        }
+        for (int i = 0; i < numeros.length; i+=2) {
+            int numerito = numeros[i];
+            numeros[i] = numeros[i+1];
+            numeros[i+1]= numerito;
+        }*/
+        int plus = 1;
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = numeros[numeros.length-plus];
 
-            /*for (int item:numeros) {
-                System.out.print(item);
-            }*/
+            plus++;
+        }
 
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i] + ", ");
         }
     }
 }
