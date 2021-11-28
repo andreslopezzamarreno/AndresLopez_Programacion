c66ee0b8cf8442fdf4ac12911ec4bebab3581625
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-28 16:09:09 +0100

    update

--- Tema 4/Ejercicios/src/FraseArray.java
+++ Tema 4/Ejercicios/src/FraseArray.java
@@ -3,17 +3,19 @@
 // sin terminar
 
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class FraseArray {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
 
-        String [] frase = new String[10];
+        String [] frase;
         System.out.println("Introduce una frase ");
 
-        for (int i = 0; i < frase.length; i++) {
-            System.out.println(frase[i]);
-        }
+        Arrays.asList(frase) = sc.next();
+        System.out.println(frase.length);
+
+
     }
 }
