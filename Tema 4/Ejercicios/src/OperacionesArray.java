b707bf99ddbc0b3af253a6d05d671771298642d0
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-11-28 16:00:19 +0100

    update

--- Tema 4/Ejercicios/src/OperacionesArray.java
+++ Tema 4/Ejercicios/src/OperacionesArray.java
@@ -1,12 +1,9 @@
-
-
-// sin terminar
-
-
 public class OperacionesArray {
     public static void main(String[] args) {
         int[] numeros = new int[10];
         int posfin;
+
+        System.out.println("El Array aleatorio es: ");
         for (int i = 0; i < numeros.length; i++) {
             numeros[i] = (int) (Math.random() * 10);
 
@@ -19,7 +16,7 @@ public class OperacionesArray {
         System.out.println();
 
 
-        /*for (int i = 0; i < numeros.length; i++) {
+        for (int i = 0; i < numeros.length; i++) {
             if (numeros[i] % 2 == 0) {
                 numeros[i]++;
             } else {
@@ -35,32 +32,53 @@ public class OperacionesArray {
             numeros[i] += (-5 + (int) (Math.random() * 11));
 
         }
-        posfin = numeros[numeros.length-1];
-        for (int i = numeros.length-1; i >= 0; i--) {
+        posfin = numeros[numeros.length - 1];
+        for (int i = numeros.length - 1; i >= 0; i--) {
             if (i != 0) {
                 numeros[i] = numeros[i - 1];
             } else {
                 numeros[0] = posfin;
             }
         }
-        for (int i = 0; i < numeros.length; i+=2) {
+        for (int i = 0; i < numeros.length; i += 2) {
             int numerito = numeros[i];
-            numeros[i] = numeros[i+1];
-            numeros[i+1]= numerito;
-        }*/
-        
+            numeros[i] = numeros[i + 1];
+            numeros[i + 1] = numerito;
+        }
+
         int plus = 1;
         for (int i = 0; i < numeros.length; i++) {
             int numerito = numeros[i];
-            if ( i <=4) {
+            if (i <= 4) {
                 numeros[i] = numeros[numeros.length - plus];
-                numeros[numeros.length-plus] = numerito;
+                numeros[numeros.length - plus] = numerito;
             }
             plus++;
         }
 
+        System.out.println("El Array resultande es: ");
         for (int i = 0; i < numeros.length; i++) {
-            System.out.print(numeros[i] + ", ");
+            if (i < numeros.length - 1) {
+                System.out.print(numeros[i] + ", ");
+            } else {
+                System.out.println(numeros[i]);
+            }
+
         }
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] % 2 == 0) {
+                System.out.println("El primer numero par es " + numeros[i]);
+                break;
+            }
+        }
+
+        for (int i = numeros.length - 1; i >= 0; i++) {
+            if (numeros[i] % 2 != 0) {
+                System.out.println("El ultimo numero impar es el " + numeros[i]);
+                break;
+            }
+        }
+
     }
 }
