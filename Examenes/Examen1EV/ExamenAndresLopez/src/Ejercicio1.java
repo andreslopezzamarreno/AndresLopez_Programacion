31c3b69cce730ebe5228a2bc9e3a8c4be9120039
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-12-01 14:27:13 +0100

    update

--- Examenes/Examen1EV/ExamenAndresLopez/src/Ejercicio1.java
+++ Examenes/Examen1EV/ExamenAndresLopez/src/Ejercicio1.java
@@ -5,31 +5,34 @@ public class Ejercicio1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nums = new int[0];
+        // int[] nums = new int[0];    no crear array con posiciones 0, crearlo nulo.
+        int[] nums = null ;
         boolean creacion = false;
         int opcion;
-        int tamaño = -1;
+        //int tamaño = -1;    no utilizar ñ
+        int tamanio = -1;
         int posInicial;
 
         do {
-            System.out.print("1.Introducir tamaño del Array \n2.Rellenar Array \n3.Sacar posiciones donde el valor sea menor que 10 y par " +
+            System.out.print("1.Introducir tamaño del Array \n2.Rellenar Array \n3.Sacar posiciones donde el valor sea mayor que 10 y par " +
                     "\n4.Rotar posiciones a la izquierda\n5.Sacar todas las posiciones del array \n6.salir\n");
             opcion = sc.nextInt();
             switch (opcion) {
                 case 1:
                     System.out.println("Introducir tamaño del array");
-                    tamaño = sc.nextInt();
-                    if (tamaño > 0) {
-                        nums = new int[tamaño];
+                    tamanio = sc.nextInt();
+                    if (tamanio > 0) {
+                        nums = new int[tamanio];
                     } else {
                         System.out.println("Error. El numero introducido es 0 o negativo");
                     }
-
                     System.out.println();
                     break;
+
                 case 2:
                     System.out.println("Rellenar array con numeros aleatorios");
-                    if (tamaño != -1) {
+
+                    if (tamanio != -1) {
                         for (int i = 0; i < nums.length; i++) {
                             nums[i] = (int) (Math.random() * 101);
                             creacion = true;
@@ -39,8 +42,9 @@ public class Ejercicio1 {
                     }
                     System.out.println();
                     break;
+
                 case 3:
-                    System.out.println("Sacar posiciones donde el valor sea menor que 10 y par ");
+                    System.out.println("Sacar posiciones donde el valor sea mayor que 10 y par ");
                     if (creacion) {
                         for (int i = 0; i < nums.length; i++) {
                             if (nums[i] > 10 && nums[i] % 2 == 0) {
@@ -52,12 +56,14 @@ public class Ejercicio1 {
                     }
                     System.out.println("\n");
                     break;
+
                 case 4:
                     if (creacion) {
                         System.out.println("Rotar las posiciones a la izquierda");
                         posInicial = nums[0];
                         for (int i = 0; i < nums.length; i++) {
-                            if (nums[i] != nums[nums.length - 1]) {
+                            //if (nums[i] != nums[nums.length - 1]) {      fallo, evaluar posicion no datos
+                            if (i != nums.length - 1) {
                                 nums[i] = nums[i + 1];
                             } else {
                                 nums[i] = posInicial;
@@ -70,6 +76,7 @@ public class Ejercicio1 {
                     }
                     System.out.println("\n");
                     break;
+
                 case 5:
                     if (creacion) {
                         for (int i = 0; i < nums.length; i++) {
@@ -83,6 +90,7 @@ public class Ejercicio1 {
                 case 6:
                     System.out.println("Salir ");
                     break;
+
                 default:
                     System.out.println("Error. Opcion no valida");
 
