b47b79ff6b72036e1d2a05d911bf0086b5f9f2e2
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2021-12-24 19:54:29 +0100

    Update AgendaHashtable.java

--- Tema 4/Ejercicios/src/baseHashtable/AgendaHashtable.java
+++ Tema 4/Ejercicios/src/baseHashtable/AgendaHashtable.java
@@ -1,7 +1,40 @@
 package baseHashtable;
 
+import java.util.Hashtable;
+import java.util.Scanner;
+
 public class AgendaHashtable {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        Hashtable<String, Object[]> agenda = new Hashtable<>();
+        int opcion;
+
+        do {
+            System.out.println("1. Agregar persona\n2. buscar persona (por DNi)\n3. Borrar persona (por DNI) \n4. Listar perssona\n5. Salir ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+                    System.out.println("Salir");
+                    break;
+            }
+        } while (opcion != 5);
     }
 }
