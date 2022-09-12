bcb98e1c3c79b1b8f299dc50e368a48b6ba2c9bc
Andrés López Zamarreño <lopez.zama@gmail.com>
2022-09-12 09:12:34 +0200

    Update Bar.java

--- Tema 2/T2/src/Bar.java
+++ Tema 2/T2/src/Bar.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Bar {
     public static void main(String args[]) {
-        
+
             Scanner in = new Scanner(System.in);
 
             //declaracion de variables
